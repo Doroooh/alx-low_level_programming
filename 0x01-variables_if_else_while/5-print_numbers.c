2d21a8b36e568f124bfbb926cf2e88a00cddb95f
Waithira <dorotttyyy@gmail.com>
2023-03-17 01:59:47 -0700

    zero to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers 0 to number 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar ("\n")
+	return (0);
+}
