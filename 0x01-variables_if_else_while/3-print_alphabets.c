44670356bc02e33ac8ee3f8398efb22e8a276111
Waithira <dorotttyyy@gmail.com>
2023-03-17 01:44:40 -0700

    alphas

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include < studio.h>
+
+/**
+ * main - print alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	    int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
