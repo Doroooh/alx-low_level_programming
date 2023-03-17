26deb0a8acdca1c7e0a8a7262882a92807a1cef5
Waithira <dorotttyyy@gmail.com>
2023-03-17 02:04:00 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print number 0 to number 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
