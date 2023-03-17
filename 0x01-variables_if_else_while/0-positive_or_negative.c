ead92814c70e9c23e36363a8085611aff7c8d55d
Waithira <dorotttyyy@gmail.com>
2023-03-17 00:37:24 -0700

    positive ot negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <studio.h>
+#include <time.h>
+#include <studio.h>
+
+/**
+ * main - Entry determines whether a number is positive, negative or a zero
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
