d2d31b8017379e23507328ffba238839c0400026
Waithira <dorotttyyy@gmail.com>
2023-03-21 12:43:49 -0700

    1-alphabet

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
   *_putchar - write character c to stdout
  * @c: the character to be printed
+ *
  * Return: 1 (success).
- * if an error -1 to be returned.
+ * on an error -1 to be returned.
  */
-int_putchar(char c)
+int _putchar(char c)
 {
         return (write(1, &c, 1));
 }
