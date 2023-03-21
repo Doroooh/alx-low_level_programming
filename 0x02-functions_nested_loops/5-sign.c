480453ffc7c05067c5fb60e848f8829637ebc2d1
Waithira <dorotttyyy@gmail.com>
2023-03-21 13:46:27 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - print sign of number
  * @n: number to check
- * Return: 1 for positive number, -1 for negative number or zero for anything else
+ * Return: 1 for positive number, -1 for negative number or zero for others
  */
 
 int print_sign(int n)
