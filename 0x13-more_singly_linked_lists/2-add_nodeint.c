52b4e863b15374f0d1cb1908bda58702ffd880d0
Waithira <dorotttyyy@gmail.com>
2023-05-02 00:57:18 -0700

    three

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * create_nodeint - creates a new node at the beginning of a linked list
- * @head: a pointer to the first node in the list
- * @value: the value to be stored in the new node
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to a pointer to the first node in the list
+ * @n: data to insert in the new node
  *
- * Returns: a pointer to the new node, or NULL if it fails
+ * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *create_nodeint(listint_t **head, const int value)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *new_node = malloc(sizeof(listint_t));
 
-	new_node = malloc(sizeof(listint_t));
-	if (!new_node)
+	if (new_node == NULL)
+	{
 		return (NULL);
-
-	new_node->n = value;
+	}
+	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
