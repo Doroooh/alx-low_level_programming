b5b84ef134f73f2ad480bbd76ea6590ba9fbe439
Waithira <dorotttyyy@gmail.com>
2023-05-08 14:49:07 -0700

    task 0 project

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,28 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - it will read the text file, and print the letters
- * @filename: this is the file to be read
- * @letters: this is the actual number of the letters to be printed.
- *
- * Return: this will print the number of letters is successful, if failure, return 0..
+ * read_textfile- It will read the text file print to STDOUT.
+ * @filename: the file that is being read
+ * @letters: this is the number of letters to be read
+ * Return: l- the actual number of bytes read and also printed
+ *        0 if it fails or if the filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters);
 {
-	int filedescriptor;
-	ssize_t nrd, nwr;
 	char *buffer;
+	ssize_t file_descriptor;
+	ssize_t l;
+	ssize_t d;
 
-	if (!filename)
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
 		return (0);
-
-	filedescriptor = open(filename, O_RDONLY);
-
-	if (filedescriptor == -1)
-		return (0);
-
-	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
-		return (0);
-
-	nrd = read(filedescriptor, buffer, letters);
-	nwr = write(STDOUT_FILENO, buffer, nrd);
-
-	close(filedescriptor);
+	buffer = malloc(sizeof(char) * letters);
+	d = read(file_descriptor, buffer, letters);
+	l = write(STDOUT_FILENO, buffer, d);
 
 	free(buffer);
-
-	return (nwr);
+	close(file_descriptor);
+	return (l);
 }
