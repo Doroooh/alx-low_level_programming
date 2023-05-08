73593db3493a14ded921240f2294d96f08ad9994
Waithira <dorotttyyy@gmail.com>
2023-05-08 14:58:07 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,27 +2,37 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- It will read the text file print to STDOUT.
- * @filename: the file that is being read
- * @letters: this is the number of letters to be read
- * Return: l- the actual number of bytes read and also printed
- *        0 if it fails or if the filename is NULL.
+ * read_textfile - reads text file and prints its content to STDOUT.
+ * @filename: name of the file to be read
+ * @letters: maximum number of letters to be read
+ *
+ * Return: The number of bytes read and printed, or 0 if the function fails or filename is NULL.
  */
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t print_file_to_stdout(const char *filename, size_t letters);
 {
-	char *buffer;
-	ssize_t file_descriptor;
-	ssize_t l;
+	int fd, a;
 	ssize_t d;
+	char *buf;
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	d = read(file_descriptor, buffer, letters);
-	l = write(STDOUT_FILENO, buffer, d);
 
-	free(buffer);
-	close(file_descriptor);
-	return (l);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
+	d = read(fd, buf, letters);
+	if (d == -1) {
+		free(buf);
+		close(fd);
+		return (0);
+	}
+	a = write(STDOUT_FILENO, buf, d);
+	free(buf);
+	close(fd);
+
+	if (a != d)
+		return (0);
+	return (a);
 }
