ef32cb2ba036f588cd37855104442fbc3192e600
Waithira <dorotttyyy@gmail.com>
2024-02-08 13:35:11 -0800

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,18 +3,22 @@
 /**
  * print_binary - this will be printing the binary representation of a number.
  *
- * @n: this is the number to be printed.
+ * @n: this is the unsigned int to be printed.
+ *
+ * Return: the converted number
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1ul << 63;
-
-	while (mask > 0)
+	if (n > 1)
 	{
-		if (n & mask)
-			_putchar('1');
-		else
-			_putchar('0');
-		mask >>= 1;
+		print_binary(n >> 1);
+	}
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
 	}
 }
