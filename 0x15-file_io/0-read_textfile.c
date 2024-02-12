dcdf06555ed8fd7ae01cecb4bbaa3fe82e373118
Waithira <dorotttyyy@gmail.com>
2024-02-12 14:48:04 -0800

    file tasks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,45 +4,30 @@
 
 /**
  * read_textfile - Will read the text file and print it to the STDOUT.
- * @filename: Name of the file being read.
+ * @filename: Name of the text file being read.
  * @letters: Number of letters to read and to be printed.
- *
- * Return: Actual number of the letters read and printed.
- *         If file cannot be opened or read, return 0.
- *         If filename is NULL, return 0.
- *         If write fails or does not write the expected amount of bytes, return 0.
+ * Return: a- actual number of the letters read and are printed.
+ *         If the file cannot be opened or read, return 0.
+ *         If the filename is NULL, return 0.
+ *         If the write fails or does not write the expected amount of bytes, return 1.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes_read = 0;
 	char *buffer;
-	FILE *file;
-	
-	if (filename == NULL)
-		return 0;
-	file = fopen(filename, "r");
-	if (file == NULL)
-		return 0;
-	
-	buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
-	{
-		fclose(file);
-		return 0;
-	}
-	
-	bytes_read = fread(buffer, sizeof(char), letters, file);
-	if (bytes_read == -1)
-	{
-		fclose(file);
-		free(buffer);
-		return 0;
-	}
+	ssize_t *file;
+	ssize_t *a;
+	ssize_t *t;
 
-	buffer[bytes_read] = '\0';
-	printf("%s", buffer);
-	
-	fclose(file);
-	free(buffer);	
-	return bytes_read;
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	t = read(file, buffer, letters);
+	a = write(STDOUT_FILENO, buffer, t);
+
+	free(buffer);
+	close(file);
+	return (a);
 }
