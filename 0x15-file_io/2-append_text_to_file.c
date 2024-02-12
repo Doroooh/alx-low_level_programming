e9455bc25c9d18b4741d5db9964d8c79a0f8e745
Waithira <dorotttyyy@gmail.com>
2024-02-12 15:41:34 -0800

    files tasks 15

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,24 +12,24 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, k, lm = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[lm];)
-			lm++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	file = open(filename, O_WRONLY | O_APPEND);
-	k = write(file, text_content, lm);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
 
-	if (file == -1 || k == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(file);
+	close(o);
 
 	return (1);
 }
