0545926fa5e5a122039f39cd8f3640a2b17560ca
Waithira <dorotttyyy@gmail.com>
2024-02-12 15:04:24 -0800

    files tasks 15

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,8 +20,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	file = open(filename, O_RDONLY);
 	if (file == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters + 1);
-	t = read(file, buffer, letters + 1);
+	buffer = malloc(sizeof(char) * letters);
+	t = read(file, buffer, letters);
 	a = write(STDOUT_FILENO, buffer, t);
 
 	free(buffer);
