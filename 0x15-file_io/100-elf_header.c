07b2be53d5443c7a74696a693ca60bee9f361ce7
Waithira <dorotttyyy@gmail.com>
2024-02-12 17:28:36 -0800

    files task 15

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -67,13 +67,6 @@ void print_magic(unsigned char *e_ident)
 	check_elf(header->e_ident);
 	printf("ELF Header:\n");
 	print_magic(header->e_ident);
-	print_class(header->e_ident);
-	print_data(header->e_ident);
-	print_version(header->e_ident);
-	print_osabi(header->e_ident);
-	print_abi(header->e_ident);
-	print_type(header->e_type, header->e_ident);
-	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
 	close_elf(o);
