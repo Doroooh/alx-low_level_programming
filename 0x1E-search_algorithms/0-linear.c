742e6c22e2d8e826df25ae583e87216ff5f38850
Waithira <dorotttyyy@gmail.com>
2024-07-17 12:35:34 -0700

    seach alg project

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,30 +2,17 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
-/**
- * linear_search - searches for a value in an array of integers
- *
- * @array: a pointer to the first element of the array to search in
- * @size: the number of elements in array
- * @value: the value to search for
- *
- * Return: the first index where value is located or -1 on failure or not found
- */
+int linear_search(int *array, size_t size, int value) {
+    if (array == NULL) {
+        return -1;
+    }
 
-int linear_search(int *array, size_t size, int value)
-{
-	size_t i = 0;
+    for (size_t i = 0; i < size; i++) {
+        printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+        if (array[i] == value) {
+            return i;
+        }
+    }
 
-	if (array)
-	{
-		while (i < size)
-		{
-			printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-				return (i);
-			i++;
-		}
-	}
-
-	return (-1);
+    return -1;
 }
